0086b08a59f8e6dae54bc1c0315a528be63cd0bf
Alex Wait <alex.wait@emberex.com>
2014-02-02 18:41:13 -0800

    View working!

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,10 +1,17 @@
 'use strict';
 
 angular.module('amazonS3TestApp')
-  .controller('MainCtrl', ["S3Service", function ($scope, S3Service) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  .controller('MainCtrl', ["$scope", "S3Service", function ($scope, S3Service) {
+    	var files = S3Service.getAllFileObjects();
+
+    	files.then(function(objects) {
+    		//what objects are there???
+    		console.log("File List:", objects);
+    		$scope.fileList = objects;
+    	});
+
+    	$scope.fileList = [{
+    		name: "Loading...",
+    		url: "http://www.google.com"
+    	}]
   }]);
--- app/scripts/services/S3Service.js
+++ app/scripts/services/S3Service.js
@@ -1,9 +1,36 @@
-s3app.service('S3Service', ["$timeout", "$q", , function ($timeout, $q) {
-	var instance = {};
+s3app.service('S3Service', ["$timeout", "$q",
+	function($timeout, $q) {
+		var instance = {};
 
 
+		var myAWS = new AWS.S3();
 
-	instance.getAllFiles = function() {
+		function getSignedUrl(key) {
+			var def = $q.defer();
+			myAWS.getSignedUrl('getObject', {Bucket:"anothertestbucketalexwait", Key: key}, function(error, url) {
+			//	console.log("url is", {name: key, url: url});
+				def.resolve({name: key, url: url});
+			});
+			return def;
+		}
 
-	};
-}]);
+		instance.getAllFileObjects = function() {
+			var def = $q.defer();
+			var defs = [];
+			myAWS.listObjects({
+				Bucket: "anothertestbucketalexwait"
+			}, function(error, data) {
+				for (var i = 0; i < data.Contents.length; i++) {
+					//console.log(data.Contents[i].Key);
+					defs.push(getSignedUrl(data.Contents[i].Key).promise);
+				}
+				var allDef = $q.all(defs);
+				allDef.then(function(allFileObjects) { 
+					def.resolve(allFileObjects);
+				})
+			});
+			return def.promise;
+		};
+		return instance;
+	}
+]);
